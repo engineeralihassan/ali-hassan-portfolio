cef9375f66effa474df314d5c6ac843725f5f3d0
Ali Hassan <engineralihasssan@gmail.com>
2026-08-20 03:45:22 +0500

    dszszdg

--- src/components/header/nav/index.tsx
+++ src/components/header/nav/index.tsx
@@ -3,7 +3,6 @@ import { motion } from "motion/react";
 import styles from "./style.module.scss";
 import { height } from "../anim";
 import Body from "./body/body";
-import Image from "./image/image";
 import MotionToggle from "@/components/motion-toggle";
 
 import { links } from "@/components/header/config";
@@ -44,10 +43,6 @@ const Index: React.FC<IndexProps> = ({ setIsActive }) => {
             setIsActive={setIsActive}
           />
         </div>
-        <Image
-          src={links[selectedLink.index].thumbnail}
-          isActive={selectedLink.isActive}
-        />
       </div>
       {/* Subtle reduced-motion control, tucked into the bottom-right of the menu.
           Stop propagation so toggling motion doesn't also close the menu. */}
